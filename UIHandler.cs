6aa2c5750ab1869638ee145353aa25758630f327
akash2237778 <akash2237778@gmail.com>
2020-04-18 00:10:35 +0530

    Auth completed : Bug can't able to handle exceptions

--- UIHandler.cs
+++ UIHandler.cs
@@ -4,25 +4,68 @@ using System.Collections.Generic;
 using Firebase.Auth;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIHandler : MonoBehaviour
 {
     Firebase.Auth.FirebaseAuth auth;
     public InputField Email;
     public InputField Password;
-    public Button CreateUser;
+    public Button SignIn;
     Firebase.Auth.FirebaseUser newUser;
+    public Button chngScene;
+    public Text signInText;
+    int retInt = 0;
 
     void Start() {
+        //SceneManager.LoadScene("GameScene");
         auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
         Debug.Log(auth);
-        CreateUser.onClick.AddListener(delegate () { createUser(Email.text, Password.text); });
-
+        SignIn.onClick.AddListener(delegate () { signIn(Email.text, Password.text); });
     }
 
 
+    public int signIn(string email, string password) {
+        signInText.text = "Start Game";
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                retInt = 0;
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                signInText.text = "Error Occured";
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                retInt = 1;
+                return;
+            }
+            if (task.IsCompleted) {
+                newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+                retInt = 2;
+            }
 
-    public void createUser(string email , string password) {
+            if (retInt < 2)
+            {
+                signInText.text = "Error Occured";
+            }
+
+        });
+
+        if (retInt == 2)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        return retInt;
+    }
+
+
+    //to Create User
+    /*public void createUser(string email , string password) {
         auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
             if (task.IsCanceled)
             {
@@ -41,9 +84,11 @@ public class UIHandler : MonoBehaviour
                 newUser.DisplayName, newUser.UserId);
         });
        //throw new NotImplementedException();
+    }*/
+
+
+    void Update() {
+
     }
-
-
-    void Update() { }
 
 }
