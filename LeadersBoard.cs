d72080ecd2e50403455b672c28c5b315e18dd8bf
akash2237778 <akash2237778@gmail.com>
2020-04-19 23:50:40 +0530

    Structure Ready

---
+++ LeadersBoard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeadersBoard : MonoBehaviour
+{
+    public GameObject Container;
+   // public GameObject Template;
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        entryContainer = Container.transform;
+        entryTemplate = entryContainer.Find("Template");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        float height = 20f;
+        for (int i = 0; i < 10; i++) {
+            Transform entryTransform = Instantiate( entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -height * i);
+            entryTransform.gameObject.SetActive(true);
+
+        }
+    }
+
+    // Update is called once per frame
+   
+}
